e7a5e36ed3db4bbde14e72fdac29e7e972b0a212
coalest <57316577+coalest@users.noreply.github.com>
2021-02-14 09:24:58 +0100

    latest problems

---
+++ exercises/rb101-rb109_small_problems/medium_1/rotation_part1.rb
@@ -0,0 +1,19 @@
+def rotate_array(arr)
+  arr_dup = arr.dup
+  arr_dup.push(arr_dup.shift)
+  arr_dup
+end
+
+# def rotate_array(arr)
+#   arr2 = arr
+#   arr2.push(arr2.shift)
+#   arr2
+# end
+
+p rotate_array([7, 3, 5, 2, 9, 1]) == [3, 5, 2, 9, 1, 7]
+p rotate_array(['a', 'b', 'c']) == ['b', 'c', 'a']
+p rotate_array(['a']) == ['a']
+
+x = [1, 2, 3, 4]
+p rotate_array(x) == [2, 3, 4, 1]   # => true
+x == [1, 2, 3, 4]                 # => true
