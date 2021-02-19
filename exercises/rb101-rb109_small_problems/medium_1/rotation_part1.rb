6244743203c1d991ea84cc8bff9c882ff92c75a4
coalest <57316577+coalest@users.noreply.github.com>
2021-02-19 15:54:25 +0100

    small problems progress

--- exercises/rb101-rb109_small_problems/medium_1/rotation_part1.rb
+++ exercises/rb101-rb109_small_problems/medium_1/rotation_part1.rb
@@ -1,14 +1,21 @@
+# def rotate_array(arr)
+#   arr2 = arr.dup
+#   arr2.push(arr2.shift)
+# end
+
 def rotate_array(arr)
-  arr_dup = arr.dup
-  arr_dup.push(arr_dup.shift)
-  arr_dup
+  arr[1..-1] + [arr[0]]
 end
 
-# def rotate_array(arr)
-#   arr2 = arr
-#   arr2.push(arr2.shift)
-#   arr2
-# end
+def rotate_string(string)
+  chars = string.chars
+  rotate_array(chars).join
+end
+
+def rotate_int(int)
+  arr_ints = int.digits.reverse
+  rotate_array(arr_ints).join
+end
 
 p rotate_array([7, 3, 5, 2, 9, 1]) == [3, 5, 2, 9, 1, 7]
 p rotate_array(['a', 'b', 'c']) == ['b', 'c', 'a']
@@ -17,3 +24,6 @@ p rotate_array(['a']) == ['a']
 x = [1, 2, 3, 4]
 p rotate_array(x) == [2, 3, 4, 1]   # => true
 x == [1, 2, 3, 4]                 # => true
+
+p rotate_string('hello world')
+p rotate_int(342)
