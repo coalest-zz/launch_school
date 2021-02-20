7cd3a34b69c7eb0717830cfcb3456e58a6f50095
coalest <57316577+coalest@users.noreply.github.com>
2021-02-20 18:06:18 +0100

    caturday

---
+++ exercises/rb101-rb109_small_problems/debugging/whats_wrong_with_the_output.rb
@@ -0,0 +1,8 @@
+arr = ["9", "8", "7", "10", "11"]
+arr.sort! do |x, y|
+    y.to_i <=> x.to_i
+  end
+p arr
+
+# Expected output: ["11", "10", "9", "8", "7"] 
+# Actual output: ["10", "11", "7", "8", "9"] 
